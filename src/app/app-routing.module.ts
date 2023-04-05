b5af5503abf8972d2635fcd28d980002029c280f
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-04-05 15:59:11 -0500

    basic nav-bar implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: NavBarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
