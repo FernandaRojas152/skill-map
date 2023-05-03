f112e6233f5ee162478b482181b7a739447b4f47
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2023-05-03 09:29:50 -0500

    hightcharts base library implemented

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import * as Highcharts from 'highcharts';
 import { Component, OnInit } from '@angular/core';
 import { Skills } from '../skills';
 import { SkillService } from '../skill.service';
